334b379f5557e13c4f600e75fdd58885065928ca
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2022-06-12 08:56:06 +0200

    :art: Fix import order with isort on docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,11 @@
 
 import os
 import sys
+
 import sphinx_rtd_theme
+
 sys.path.insert(0, os.path.abspath('../..'))
 import ensightreader
-
 
 # -- Project information -----------------------------------------------------
 
